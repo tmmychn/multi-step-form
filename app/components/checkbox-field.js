5fcff67ffdac66d69ab495d55033caae775641ca
Tommy Chen <tommy.cxy@growthops.asia>
2023-10-04 14:38:20 +0800

    Feat: refactor checkbox

--- app/components/checkbox-field.js
+++ app/components/checkbox-field.js
@@ -3,10 +3,16 @@ export function CheckboxField({fieldData, errorMsg}) {
         <div className="flex flex-col gap-4">
             <div className="flex flex-col gap-4 justify-between">
                 {fieldData.map((card, index) => (
-                    <div key={card.label} className="w-full p-4 border border-black rounded-lg">
-                        <input type="checkbox" id={`checkbox ${index}`} name="vehicle1" value={card.label}></input>
-                        <p className="text-lg">{card.label}</p>
-                        <p>{card.description}</p>
+                    <div 
+                        key={card.label} 
+                        className="w-full flex justify-between items-center p-4 border border-black rounded-lg"
+                    >
+                        <input type="checkbox" name={`checkbox ${index}`} value={card.label} />
+                        <div>
+                            <p className="text-lg">{card.label}</p>
+                            <p>{card.description}</p>
+                        </div>
+                        <p>{card.price}</p>
                     </div>
                 ))}
             </div>
